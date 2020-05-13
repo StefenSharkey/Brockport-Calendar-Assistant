5bfa3581d4f4ce1b67de23c19d68ff01f82576ad
Stefen Sharkey <stefensharkey@gmail.com>
2020-05-13 13:14:18 -0400

    Remove unused import.

--- src/main/java/edu/brockport/voiceassistant/PaychexBrockportCalendarApp.java
+++ src/main/java/edu/brockport/voiceassistant/PaychexBrockportCalendarApp.java
@@ -29,7 +29,6 @@ import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;

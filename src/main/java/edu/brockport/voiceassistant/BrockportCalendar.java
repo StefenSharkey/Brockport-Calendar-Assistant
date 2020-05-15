f52b09244563548731b5c639593ebd2129e918c6
Stefen Sharkey <stefensharkey@gmail.com>
2020-05-15 11:50:15 -0400

    Document code.

--- src/main/java/edu/brockport/voiceassistant/BrockportCalendar.java
+++ src/main/java/edu/brockport/voiceassistant/BrockportCalendar.java
@@ -67,10 +67,40 @@ public class BrockportCalendar {
     }
 
     /**
+     * Inserts the given {@link edu.brockport.voiceassistant.DateInfo} into {@code DATES} for later usage if it is more
+     * similar than {@code DATE_SIMILARITY_THRESHOLD}, storing no more than {@code MAX_DATES} dates at any given time.
+     * <p>
+     * {@code DATES} will always be stored in descending similarity.
+     *
+     * @param dateInfo The date info to store.
+     */
+    private void insertDate(DateInfo dateInfo) {
+        DATES.sort(Comparator.comparing(o -> ((DateInfo) o).getSimilarity()).reversed());
+
+        // Continue if the similarity is equal to or greater than the preset threshold.
+        if (dateInfo.getSimilarity() >= DATE_SIMILARITY_THRESHOLD) {
+            // If the number of dates stored is less than the max dates, add it.
+            // Otherwise, set the least similar date with this new date info if new date is more similar than the least
+            // similar existing date.
+            if (DATES.size() < MAX_DATES) {
+                DATES.add(dateInfo);
+            } else {
+                int lastIndex = DATES.size() - 1;
+
+                if (dateInfo.getSimilarity().compareTo(DATES.get(lastIndex).getSimilarity()) >= 0) {
+                    DATES.set(lastIndex, dateInfo);
+                }
+            }
+        }
+
+        DATES.sort(Comparator.comparing(o -> ((DateInfo) o).getSimilarity()).reversed());
+    }
+
+    /**
      * Formats a string of all date and time variants that the Brockport calendar uses into a format parsable by
      * {@link java.util.Date}. If a date range is detected, multiple strings will be formatted.
-     *
-     * The following are examples of formats that are understood:
+     * <p>
+     * The following are examples of understood formats:
      * July 4, 2020
      * August 23, 2019, Friday
      * September 26 – 28, 2019
@@ -117,6 +147,7 @@ public class BrockportCalendar {
                                                                               dateSplit.get(3),
                                                                               dateSplit.get(4)));
 
+                    // Loop through each date within the range and add them to the list.
                     while (!startDate.after(endDate)) {
                         dates.add(startDate);
                         startDate = DateUtils.addDays(startDate, 1);
@@ -130,7 +161,7 @@ public class BrockportCalendar {
                     break;
                 case 9:
                     // e.g. April 10, 2020, Friday, 9 AM – 5 PM
-                    // Current method: chop off the second time and run it through like normal.
+                    // Current method: chop off the time and run it through like normal.
                     dateSplit = dateSplit.subList(0, 3);
                     dateFormat = new SimpleDateFormat("MMMMM d, yyyy");
                     dates.add(dateFormat.parse(String.join(" ", dateSplit)));
@@ -154,9 +185,10 @@ public class BrockportCalendar {
      * {@link java.util.ArrayList<java.util.Date>}, for an event, since multiple events with the same name can occur and
      * event name matching may not be ideal.
      *
-     * @param eventName The event name.
-     * @param tense If {@code Tense.PAST}, considers any past events. If {@code Tense.NOTPAST}, only future events are
-     *             considered.
+     * @param eventName       The event name.
+     * @param tense           If {@code Tense.PAST}, considers any past events. If {@code Tense.NOTPAST}, only future events are
+     *                        considered.
+     * @param cleanEventNames Whether or not the names should be cleaned of unnecessary information.
      * @return The {@link java.util.ArrayList<java.util.Date>} for an event.
      */
     public List<DateInfo> getEventDates(String eventName, Tense tense, boolean cleanEventNames) {
@@ -170,6 +202,7 @@ public class BrockportCalendar {
             // Remove all non-alphanumeric characters from the current event.
             String tempEvent = currEventName.toLowerCase().replaceAll("[^a-z0-9]", "");
 
+            // If we are looking at past events or if the event has not yet occurred, insert the date.
             if (tense == Tense.PAST || !date.before(new Date())) {
                 insertDate(new DateInfo(cleanEventNames ? getCleanEventName(currEventName) : currEventName,
                         date,
@@ -183,9 +216,10 @@ public class BrockportCalendar {
     /**
      * Retrieves the event name for a given {@link java.util.Date}, considering only the date.
      *
-     * @param eventDate The {@link java.util.Date} to consider.
+     * @param eventDate      The {@link java.util.Date} to consider.
+     * @param cleanEventName Whether or not the name should be cleaned of unnecessary information.
      * @return The name of the event.
-     *         null if no event is found.
+     * null if no event is found.
      */
     public String getEventName(Date eventDate, boolean cleanEventName) {
         final ArrayList<String> events = new ArrayList<>();
@@ -198,9 +232,11 @@ public class BrockportCalendar {
             }
         });
 
+        // Handle String concatenation in standard English conventions, including an Oxford comma.
         for (int i = 0; i < events.size(); i++) {
             if (i != 0) {
                 if (i == events.size() - 1) {
+                    // Oxford comma
                     if (events.size() > 2) {
                         ret += ",";
                     }
@@ -221,30 +257,29 @@ public class BrockportCalendar {
         return ret.isEmpty() ? null : ret;
     }
 
-    public DateInfo getDaysUntilEvent(String eventName, boolean cleanEventNames) {
-        List<DateInfo> dates = getEventDates(eventName, Tense.NOTPAST, cleanEventNames);
+    /**
+     * Retrieves the number of days until the event with {@param eventName} occurs. Returns the most similar event found
+     * wrapped in a {@link edu.brockport.voiceassistant.DateInfo} object.
+     *
+     * @param eventName      The name of the event.
+     * @param cleanEventName Whether or not the name should be cleaned of unnecessary information.
+     * @return The DateInfo for the event.
+     * null if none are found.
+     */
+    public DateInfo getDaysUntilEvent(String eventName, boolean cleanEventName) {
+        List<DateInfo> dates = getEventDates(eventName, Tense.NOTPAST, cleanEventName);
 
         return dates.isEmpty() ? null : dates.get(0);
     }
 
-    private void insertDate(DateInfo dateInfo) {
-        DATES.sort(Comparator.comparing(o -> ((DateInfo) o).getSimilarity()).reversed());
-
-        if (dateInfo.getSimilarity() >= DATE_SIMILARITY_THRESHOLD) {
-            if (DATES.size() < MAX_DATES) {
-                DATES.add(dateInfo);
-            } else {
-                int lastIndex = DATES.size() - 1;
-
-                if (dateInfo.getSimilarity().compareTo(DATES.get(lastIndex).getSimilarity()) >= 0) {
-                    DATES.set(lastIndex, dateInfo);
-                }
-            }
-        }
-
-        DATES.sort(Comparator.comparing(o -> ((DateInfo) o).getSimilarity()).reversed());
-    }
-
+    /**
+     * Retrieves the {@link java.util.List<edu.brockport.voiceassistant.DateInfo>} of events that are occurring within
+     * the next {@param numDays}.
+     *
+     * @param numDays         The number of days to search.
+     * @param cleanEventNames Whether or not the names should be cleaned of unnecessary information.
+     * @return The dates occurring within the desired time span. Sorted from soonest to latest date.
+     */
     public List<DateInfo> getEventsInNextNDays(int numDays, boolean cleanEventNames) {
         Calendar calendar = Calendar.getInstance();
 
@@ -254,6 +289,7 @@ public class BrockportCalendar {
 
         List <DateInfo> eventsInRange = new ArrayList<>();
 
+        // Loop through every date, only adding days that occur within the current day and cutoff day.
         CALENDAR.forEach((eventName, date) -> {
             if(date.after(today) && date.before(cutoff)) {
                 eventsInRange.add(new DateInfo(cleanEventNames ? getCleanEventName(eventName) : eventName, date, 0));
@@ -264,6 +300,13 @@ public class BrockportCalendar {
         return eventsInRange;
     }
 
+    /**
+     * Clean the event name by removing unnecessary information from the given event name.
+     * Specifically, removes day information and parentheses.
+     *
+     * @param eventName The event name to clean.
+     * @return The cleaned event name.
+     */
     private static String getCleanEventName(String eventName) {
         return eventName.replaceAll("Day \\d", "").replaceAll("[ ][(]\\d[)]", "").replaceAll("[(]\\d[)]", "");
     }
